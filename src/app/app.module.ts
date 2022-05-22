26619d40abf0235a03db652d031b39bd6a6a4b30
LiliLG13 <lopezlili.13.llg@gmail.com>
2022-05-22 08:50:08 -0500

    CreaciónComponentes
    Creación de los componentes principales para el funcionamiento de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrdenesComponent } from './ordenes/ordenes.component';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
+import { LoginComponent } from './login/login.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    OrdenesComponent,
+    ProveedoresComponent,
+    LoginComponent,
+    EmpleadosComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ordenes/ordenes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordenes',
+  templateUrl: './ordenes.component.html',
+  styleUrls: ['./ordenes.component.css']
+})
+export class OrdenesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
